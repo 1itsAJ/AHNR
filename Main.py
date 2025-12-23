4befc768ec390cf976a560f2d4b36a0be62a520d
1itsAJ <shamsaljashamy@gmail.com>
2025-12-23 22:13:24 +0300

    Merge

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import Train
 import UserTest
 
-Train.start(
-    "Dataset",            # Dataset path
-    "AHNRmodelCNN.keras"     # Save model
-)
+# Train.start(
+#     "Dataset",            # Dataset path
+#     "AHNRmodelCNN.keras"     # Save model
+# )
 
-# UserTest.Start(
-#     "file1.png",             # Test folder path
-#     "AHNRmodel.keras"       # Load model
-#     )
+UserTest.Start(
+    "file4.png",             # Test folder path
+    "AHNRmodelCNN.keras"       # Load model
+    )
